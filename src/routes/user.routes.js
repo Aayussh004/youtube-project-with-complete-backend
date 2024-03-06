3025845b5e07d7fc9c721abbcb642a98022db3e5
Ayush <ayushsuryawanshi004@gmail.com>
2024-03-06 14:36:58 +0530

    added routes of updates in user.routes.js

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { loginUser, logoutUser, refreshAccessToken, registerUser } from "../controllers/user.controller.js";
+import { changeCurrentPassword, getCurrentUser, getUserChannelProfile, getWatchHistory, loginUser, logoutUser, refreshAccessToken, registerUser, updateAccountDetails, updateUserAvatar, updateUserCoverImage } from "../controllers/user.controller.js";
 import { upload } from "../middleware/multer.js";
 import { verifyJWT } from "../middleware/auth.middleware.js";
 
@@ -11,23 +11,46 @@ router.route("/register").post(
     //now before registering user we will upload the data in our local server using middleware multer (upload method) humse milke jana 
     upload.fields([
         {
-            name:"avatar",
-            maxCount:1
-        },{
-            name:"coverImage",
-            maxCount:1
+            name: "avatar",
+            maxCount: 1
+        }, {
+            name: "coverImage",
+            maxCount: 1
         }
-    ]) , 
+    ]),
     registerUser
-    )//yha hum post method call kr rhe hai
+)//yha hum post method call kr rhe hai
 
 router.route("/login").post(loginUser)
 
 //secured routes
-router.route("/logout").post(verifyJWT,logoutUser)
+router.route("/logout").post(verifyJWT, logoutUser)
 
 //for creating new refreshtoken for user when they hit this endpoint
 router.route("/refreshAccessToken").post(refreshAccessToken)
+
+//for change password
+router.route("/change-password").post(verifyJWT, changeCurrentPassword)
+
+//get current user
+router.route("/current-user").get(verifyJWT, getCurrentUser)//get kyuki sirf info le rhe hai
+
+//update account details
+router.route("update-account").patch(verifyJWT, updateAccountDetails)//kyuki kuch details hi update krni hai
+
+//update avatar
+//now yha multer bhi lagega kyuki avatar ko upload krna h
+router.route("/update-avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar);
+
+//update coverImage
+router.route("update-coverImage").patch(verifyJWT, upload.single("/coverImage"), updateUserCoverImage);
+
+//get channel profile kyuki params se le rhe hai
+router.route("/c/:userName").get(verifyJWT, getUserChannelProfile)
+
+//get user watchHistory
+router.route("/history").get(verifyJWT, getWatchHistory)
+
 
 // http://localhost:8000/api/v1/users/register to ab ye registerUser call krdega jo ki message "Ok registerd" de dega
 //now download postman and create new workspace and give this same link
